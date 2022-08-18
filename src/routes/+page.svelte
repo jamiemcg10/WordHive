703b1d49c63e8ba4e7b0430de5e7d904a172c7f1
jamiemcg10 <jamie.smart.10@gmail.com>
2022-08-18 00:42:48 -0400

    add Cell

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-    import Cell from '$lib/components/Cell.svelte'
-
+  import Cell from '$lib/components/Cell.svelte'
 </script>
-<!-- #8f3302 -->
-<div>
-    <h1>Word Hive</h1>
-    <Cell />
 
+<div>
+  <h1 class="text-white underline">Word Hive</h1>
+  <Cell />
 </div>
